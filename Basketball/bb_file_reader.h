8423a33c18492c09f9910bcbf4333bd262515d06
michaelobi99 <michaelobi54@gmail.com>
2025-03-17 15:41:32 +0100

    restructured files

---
+++ Basketball/bb_file_reader.h
@@ -0,0 +1,276 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <random>
+#include <ranges>
+#include "bb_predictors.h"
+
+#ifdef _MSC_VER
+#include <Windows.h>
+#endif // _MSC_VER
+
+
+
+std::string trim(const std::string& str) {
+	auto start = std::find_if_not(str.begin(), str.end(), [](unsigned char ch) {
+		return std::isspace(ch);
+		});
+
+	auto end = std::find_if_not(str.rbegin(), str.rend(), [](unsigned char ch) {
+		return std::isspace(ch);
+		}).base();
+
+		return (start < end) ? std::string(start, end) : "";
+}
+
+
+
+std::tuple<std::string, std::vector<int>, std::vector<int>> split_string(const char* str, size_t length) {
+	std::istringstream stream(str);
+
+	std::string name;
+	std::getline(stream, name, ':');
+	name = trim(name);
+
+	std::string past_scores_str{ "" };
+	std::getline(stream, past_scores_str, ':');
+	past_scores_str = trim(past_scores_str);
+
+	std::string h2h_scores_str{ "" };
+	std::getline(stream, h2h_scores_str, ':');
+
+
+	auto to_int_vec = [](std::string& str) {
+		std::vector<int> vec;
+		std::string s{ "" };
+		for (int i = 0; i < str.size(); ++i) {
+			if (isdigit(str[i]))
+				s.push_back(str[i]);
+			else {
+				if (s.size() > 0) {
+					vec.push_back(std::stoi(s));
+					s.clear();
+				}
+			}
+		}
+		if (s.size() > 0) vec.push_back(std::stoi(s));
+		return vec;
+		};
+
+	std::vector<int> past_scores = to_int_vec(past_scores_str);
+	std::vector<int> h2h_scores = (h2h_scores_str.size() > 0) ? to_int_vec(h2h_scores_str) : std::vector<int>{};
+	return std::tuple(name, past_scores, h2h_scores);
+}
+
+
+void get_recommendation(float pred_1, float pred_2, float pred_3, float low, float high) {
+	const int max_pred_diff = 10;
+	const int max_dist_range = 20;
+	float avg_points = 0.3f * pred_1 + 0.4f * pred_2 + 0.3f * pred_3;
+	bool close_prediction = std::abs(pred_1 - pred_2) < max_pred_diff && std::abs(pred_1 - pred_3) < max_pred_diff && std::abs(pred_2 - pred_3) < max_pred_diff;
+	bool tight_range = (int)(high - low) <= max_dist_range;
+	if (close_prediction && tight_range) {
+		printf("RECOMMENDATION: Very high confidence - (close predictions, tight range) Expect about %.2f total points.\n\n", avg_points);
+	}
+	else if (close_prediction && !tight_range) {
+		printf("RECOMMENDATION: High confidence - (close predictions, wide range) Expect about %.2f total points.\n\n", avg_points);
+	}
+	else if (!close_prediction && tight_range) {
+		printf("RECOMMENDATION: High confidence - (wide predictions, tight range) Expect about %.2f total points.\n\n", avg_points);
+	}
+	else {
+		printf("RECOMMENDATION: Low confidence - (wide predictions, wide range) Expect about %.2f total points.\n\n", avg_points);
+	}
+}
+
+
+void process_basketball_file(std::string const& file_path) {
+	std::string match_details;
+	std::string match_string;
+	std::string line;
+	std::vector<int> scores;
+	std::fstream file{ file_path, std::ios::in | std::ios::binary };
+	if (!file.is_open()) {
+		std::cerr << "Failed to open file\n";
+		exit(1);
+	}
+	std::ostringstream stream;
+
+	float recommendation{ 0.0 };
+
+	//This loop reads all non-empty lines in the file. Three lines in a single loop.
+	while (!file.eof()) {
+		//Read until a non-empty line is found
+		while (std::getline(file, line) && line.size() <= 1) continue;
+		if (file.eof()) break;
+
+		auto [home_team_name, home_past_scores, home_h2h_scores] = split_string(line.c_str(), line.size());
+		match_string += home_team_name;
+
+		line.clear();
+		while (std::getline(file, line) && line.size() <= 1) continue;
+
+		auto [away_team_name, away_past_scores, away_h2h_scores] = split_string(line.c_str(), line.size());
+		match_string += " vs " + away_team_name;
+
+		line.clear();
+		while (std::getline(file, match_details) && match_details.size() <= 1) continue;
+
+		//..........................................................................................................................
+		//Exponential Smoothing
+		float home_exp_pred = exponential_smoothing(home_past_scores) * ((home_h2h_scores.size() == 0) ? 1.0 : 0.6) + mean(home_h2h_scores) * 0.4;
+		float away_exp_pred = exponential_smoothing(away_past_scores) * ((away_h2h_scores.size() == 0) ? 1.0 : 0.6) + mean(away_h2h_scores) * 0.4;
+
+		//Simple Linear Regression
+		float home_regression_pred = simple_linear_regression(home_past_scores) * ((home_h2h_scores.size() == 0) ? 1.0 : 0.6) + mean(home_h2h_scores) * 0.4;
+		float away_regression_pred = simple_linear_regression(away_past_scores) * ((away_h2h_scores.size() == 0) ? 1.0 : 0.6) + mean(away_h2h_scores) * 0.4;
+
+
+		std::copy(home_h2h_scores.begin(), home_h2h_scores.end(), std::back_inserter(home_past_scores));
+		std::copy(away_h2h_scores.begin(), away_h2h_scores.end(), std::back_inserter(away_past_scores));
+
+		float home_mean = mean(home_past_scores);
+		float home_stddev = standard_deviation(home_past_scores, home_mean);
+		float away_mean = mean(away_past_scores);
+		float away_stddev = standard_deviation(away_past_scores, away_mean);
+		float mean_total = home_mean + away_mean;
+
+		float home_lower{ 0 }, away_lower{ 0 };
+		float home_upper{ 0 }, away_upper{ 0 };
+
+		if (home_past_scores.size() > 30 || away_past_scores.size() > 30) {
+			//Normal distribution calculation
+			std::tie(home_lower, home_upper) = z_dist(home_past_scores.size(), home_mean, home_stddev);
+			std::tie(away_lower, away_upper) = z_dist(away_past_scores.size(), away_mean, away_stddev);
+		}
+		else {
+			//T-distribution calculation
+			std::tie(home_lower, home_upper) = t_dist(home_past_scores.size(), home_mean, home_stddev);
+			std::tie(away_lower, away_upper) = t_dist(away_past_scores.size(), away_mean, away_stddev);
+		}
+
+
+		//ARIMA
+		/*double arima_home_pred = predictARIMA(home_score);
+		double arima_away_pred = predictARIMA(away_score);*/
+
+		//.......................................................................................................................
+
+#ifdef _MSC_VER
+		std::random_device rd{};
+		auto mtgen = std::mt19937{ rd() };
+		auto ud = std::uniform_int_distribution<int>{ 1, 7 };
+		int random_number = ud(mtgen);
+		int text_color = 0;
+		switch (random_number) {
+		case 1: text_color = 5;
+			break;
+		case 2: text_color = 6;
+			break;
+		case 3: text_color = 9;
+			break;
+		case 4: text_color = 10;
+			break;
+		case 5: text_color = 12;
+			break;
+		case 6: text_color = 13;
+			break;
+		case 7: text_color = 14;
+			break;
+		}
+		HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+		SetConsoleTextAttribute(hConsole, text_color | FOREGROUND_INTENSITY);
+#endif // Change console color
+
+		//Display results
+		std::string design(match_details.size() + match_string.size() + 5, '.');
+		std::cout << design << "\n";
+		std::cout << match_string << " - " << match_details << "\n";
+		std::cout << design << "\n";
+
+		const char* str1 = "Likely Score Range";
+		std::cout << std::setw(38) << std::right << str1 << "\n";
+
+		//Home
+		stream << "Home: " << home_mean;
+		printf("%-20s", stream.str().c_str());
+		stream.str("");
+		stream << "(" << home_lower << " - " << home_upper << ")";
+		printf("%s\n", stream.str().c_str());
+		stream.str("");
+
+		//Away
+		stream << "Away: " << away_mean;
+		printf("%-20s", stream.str().c_str());
+		stream.str("");
+		stream << "(" << away_lower << " - " << away_upper << ")";
+		printf("%s\n", stream.str().c_str());
+		stream.str("");
+
+
+		//H2H
+		stream << "H2H : " << mean_total;
+		printf("%-20s", stream.str().c_str());
+		stream.str("");
+		stream << "(" << home_lower + away_lower << " - " << home_upper + away_upper << ")";
+		printf("%s\n\n", stream.str().c_str());
+		stream.str("");
+
+		stream << "Exponential smoothing\n";
+		stream << "Home : " << home_exp_pred << "\nAway : " << away_exp_pred << "\nTotal: " << home_exp_pred + away_exp_pred << "\n\n";
+
+		stream << "Linear regression\n";
+		stream << "Home : " << home_regression_pred << "\nAway : " << away_regression_pred << "\nTotal: " << home_regression_pred + away_regression_pred << "\n\n";
+
+
+		/*stream << "ARIMA\n";
+		stream << "Home : " << arima_home_pred << "\nAway : " << arima_away_pred << "\nTotal: " << arima_home_pred + arima_away_pred << "\n\n\n";*/
+
+
+		printf("%s", stream.str().c_str());
+
+
+		// Determine max size for iteration
+		size_t a = home_h2h_scores.size();
+		size_t b = away_h2h_scores.size();
+		if (a > 0) {
+			size_t max_h2h_size = a;
+
+			// Print table title
+			printf("Recent H2H results\n");
+
+			// Print column headers
+			printf("%-15s %-15s %-15s\n", "Home", "Away", "Total");
+
+			// Print horizontal separator
+			printf("%.*s\n", 45, "---------------------------------------------");
+
+			// Print scores
+			for (size_t i = 0; i < max_h2h_size; i++) {
+				int home_score = (i < home_h2h_scores.size()) ? home_h2h_scores[i] : 0;
+				int away_score = (i < away_h2h_scores.size()) ? away_h2h_scores[i] : 0;
+				int total = home_score + away_score;
+
+				if (home_score > 0 || away_score > 0) {
+					printf("%-15d %-15d %-15d\n", home_score, away_score, total);
+				}
+			}
+
+			std::cout << std::endl;
+		}
+
+		get_recommendation(home_mean + away_mean, home_exp_pred + away_exp_pred, home_regression_pred + away_regression_pred, home_lower + away_lower, home_upper + away_upper);
+
+		stream.str("");
+		stream.clear();
+
+		match_string.clear();
+		match_details.clear();
+#ifdef _MSC_VER
+		SetConsoleTextAttribute(hConsole, 7);
+#endif // _MSC_VER
+	}//while
+
+	file.close();
+}
---
+++ Basketball/bb_predictors.h
@@ -0,0 +1,220 @@
+#pragma once
+#include <vector>
+#include <cmath>
+#include <tuple>
+#include <iomanip>
+#include <sstream>
+#include <string>
+
+
+
+//...........................................................................................................................................................
+//Mean and standard deviation
+float mean(const std::vector<int>& scores) {
+	if (scores.empty()) return 0;
+	float result{ 0. };
+	int size = scores.size();
+	for (unsigned i{ 0 }; i < size; ++i) {
+		result += scores[i];
+	}
+	result /= float(size);
+	return result;
+}
+
+float standard_deviation(const std::vector<int>& scores, float mean) {
+	if (scores.empty()) return 0;
+	float sum_squared_diff{ 0.0 };
+	int size = scores.size();
+	for (unsigned i{ 0 }; i < size; ++i) {
+		sum_squared_diff += std::pow((mean - scores[i]), 2);
+	}
+	float stddev = std::sqrt(sum_squared_diff / (float)size - 1);
+	return stddev;
+}
+//............................................................................................................................................................
+
+
+
+
+//............................................................................................................................................................
+//Exponential smoothing
+#define SMOOTHING_FACTOR 0.3
+
+float exponential_smoothing(const std::vector<int>& scores) {
+	if (scores.size() == 0) return 0;
+	std::vector<int> scores_reversed(std::rbegin(scores), std::rend(scores));
+	int N = scores_reversed.size();
+	float current_smoothed_value{ 0.0 }, previous_smoothed_value{ 0.0 };
+	previous_smoothed_value = scores_reversed[0];
+	for (int i = 1; i < N; ++i) {
+		current_smoothed_value = SMOOTHING_FACTOR * scores[i] + (1 - SMOOTHING_FACTOR) * previous_smoothed_value;
+		previous_smoothed_value = current_smoothed_value;
+	}
+	return current_smoothed_value;
+}
+
+
+
+float adaptive_exponential_smoothing(const std::vector<int>& scores, float initialAlpha = 0.5) {
+	std::pair<float, float> alphaRange = { 0.1, 0.9 };
+	// How much to adjust alpha when there's an error
+	double adjustmentFactor = 0.1;
+
+	// Handle edge cases
+	if (scores.empty()) return 0.0;
+	if (scores.size() == 1) return scores[0];
+
+	float current_smoothed_value = scores[0];
+	float alpha = initialAlpha;
+
+	// Process each score
+	for (int t = 1; t < scores.size(); ++t) {
+		double error = scores[t] - current_smoothed_value;
+
+		// Calculate new smoothed value
+		current_smoothed_value = alpha * scores[t] + (1 - alpha) * current_smoothed_value;
+
+		if (error != 0) {
+			alpha += adjustmentFactor * (error > 0 ? 1 : -1);
+			alpha = std::max(alphaRange.first, std::min(alphaRange.second, alpha));
+		}
+	}
+
+	return current_smoothed_value;
+}
+
+
+std::vector<int> moving_median_smoother(std::vector<int> const& scores, int window = 3) {
+	size_t length = scores.size() - window + 1;
+	std::vector<int> smoothed_scores(length);
+	size_t mid = window / 2;
+	for (int i{ 0 }; i < length; ++i) {
+		std::vector<int> window_data(scores.begin() + i, scores.begin() + i + window);
+		std::sort(window_data.begin(), window_data.end());
+		smoothed_scores[i] = window_data[mid];
+	}
+	return smoothed_scores;
+}
+//......................................................................................................................................................
+
+
+
+//......................................................................................................................................................
+//Linear regression
+
+float simple_linear_regression(const std::vector<int>& scores) {
+	if (scores.empty()) return 0;
+	if (scores.size() < 2) return scores[0];
+
+	int N = scores.size() - 1; // One less for pairs of (x,y)
+	std::vector<int> x(scores.rbegin(), scores.rend() - 1); // previous scores
+	std::vector<int> Y(scores.rbegin() + 1, scores.rend()); // next scores
+
+	float sum_x = 0.0, sum_y = 0.0, sum_xy = 0.0, sum_x2 = 0.0;
+	for (int i = 0; i < N; i++) {
+		sum_x += x[i];
+		sum_y += Y[i];
+		sum_xy += x[i] * Y[i];
+		sum_x2 += x[i] * x[i];
+	}
+
+	float denominator = (N * sum_x2 - sum_x * sum_x);
+	if (std::abs(denominator) < 1e-6) { // If no clear trend, return last score
+		return x[0];
+	}
+
+	float slope = (N * sum_xy - sum_x * sum_y) / denominator;
+	float y_intercept = (sum_y - slope * sum_x) / N;
+
+	// Use most recent score (Y[N-1] since vector is reversed) for prediction
+	return (slope * Y[N - 1]) + y_intercept;
+}
+
+//Account for home and away features
+float multiple_linear_regression() {
+
+}
+//..........................................................................................................................................................
+
+
+//.........................................................................................................................................................
+//Z-Distribution
+std::tuple<float, float> z_dist(size_t n, float mean, float stddev, double z_score = 1.96) {
+	// Calculate standard error
+	double std_error = stddev / std::sqrt(n);
+	// Calculate range
+	float low = mean - (z_score * std_error);
+	float high = mean + (z_score * std_error);
+	return std::tuple{ low, high };
+}
+//.........................................................................................................................................................
+
+
+//..........................................................................................................................................................
+//T-Distribution
+
+//t_table[degree_of_freedom][tail_probability]
+float t_table[30][11] = {
+	{0.000, 1.000, 1.376, 1.963, 3.078, 6.314, 12.71, 31.82, 63.66, 318.31, 636.62},
+	{0.000, 0.816, 1.061, 1.386, 1.886, 2.920, 4.303, 6.965, 9.925, 22.327, 31.599},
+	{0.000, 0.765, 0.978, 1.250, 1.638, 2.353, 3.182, 4.541, 5.841, 10.215, 12.924},
+	{0.000, 0.741, 0.941, 1.190, 1.533, 2.132, 2.776, 3.747, 4.604, 7.173, 8.610},
+	{0.000, 0.727, 0.920, 1.156, 1.476, 2.015, 2.571, 3.365, 4.032, 5.893, 6.869},
+	{0.000, 0.718, 0.906, 1.134, 1.440, 1.943, 2.447, 3.143, 3.707, 5.208, 5.959},
+	{0.000, 0.711, 0.896, 1.119, 1.415, 1.895, 2.365, 2.998, 3.499, 4.785, 5.408},
+	{0.000, 0.706, 0.889, 1.108, 1.397, 1.860, 2.306, 2.896, 3.355, 4.501, 5.041},
+	{0.000, 0.703, 0.883, 1.100, 1.383, 1.833, 2.262, 2.821, 3.250, 4.297, 4.781},
+	{0.000, 0.700, 0.879, 1.093, 1.372, 1.812, 2.228, 2.764, 3.169, 4.144, 4.587},
+	{0.000, 0.697, 0.876, 1.088, 1.363, 1.796, 2.201, 2.718, 3.106, 4.025, 4.437},
+	{0.000, 0.695, 0.873, 1.083, 1.356, 1.782, 2.179, 2.681, 3.055, 3.930, 4.318},
+	{0.000, 0.694, 0.870, 1.079, 1.350, 1.771, 2.160, 2.650, 3.012, 3.852, 4.221},
+	{0.000, 0.692, 0.868, 1.076, 1.345, 1.761, 2.145, 2.624, 2.977, 3.787, 4.140},
+	{0.000, 0.691, 0.866, 1.074, 1.341, 1.753, 2.131, 2.602, 2.947, 3.733, 4.073},
+	{0.000, 0.690, 0.865, 1.071, 1.337, 1.746, 2.120, 2.583, 2.921, 3.686, 4.015},
+	{0.000, 0.689, 0.863, 1.069, 1.333, 1.740, 2.110, 2.567, 2.898, 3.646, 3.965},
+	{0.000, 0.688, 0.862, 1.067, 1.330, 1.734, 2.101, 2.552, 2.878, 3.610, 3.922},
+	{0.000, 0.688, 0.861, 1.066, 1.328, 1.729, 2.093, 2.539, 2.861, 3.579, 3.883},
+	{0.000, 0.687, 0.860, 1.064, 1.325, 1.725, 2.086, 2.528, 2.845, 3.552, 3.850},
+	{0.000, 0.686, 0.859, 1.063, 1.323, 1.721, 2.080, 2.518, 2.831, 3.527, 3.819},
+	{0.000, 0.686, 0.858, 1.061, 1.321, 1.717, 2.074, 2.508, 2.819, 3.505, 3.792},
+	{0.000, 0.685, 0.858, 1.060, 1.319, 1.714, 2.069, 2.500, 2.807, 3.485, 3.768},
+	{0.000, 0.685, 0.857, 1.059, 1.318, 1.711, 2.064, 2.492, 2.797, 3.467, 3.745},
+	{0.000, 0.684, 0.856, 1.058, 1.316, 1.708, 2.060, 2.485, 2.787, 3.450, 3.725},
+	{0.000, 0.684, 0.856, 1.058, 1.315, 1.706, 2.056, 2.479, 2.779, 3.435, 3.707},
+	{0.000, 0.684, 0.855, 1.057, 1.314, 1.703, 2.052, 2.473, 2.771, 3.421, 3.690},
+	{0.000, 0.683, 0.855, 1.056, 1.313, 1.701, 2.048, 2.467, 2.763, 3.408, 3.674},
+	{0.000, 0.683, 0.854, 1.055, 1.311, 1.699, 2.045, 2.462, 2.756, 3.396, 3.659},
+	{0.000, 0.683, 0.854, 1.055, 1.310, 1.697, 2.042, 2.457, 2.750, 3.385, 3.646}
+};
+
+int getTailProbabilityIndex(std::string confidence_level) {
+	if (confidence_level.starts_with("0.0")) return 0;
+	if (confidence_level.starts_with("0.5")) return 1;
+	if (confidence_level.starts_with("0.6")) return 2;
+	if (confidence_level.starts_with("0.7")) return 3;
+	if (confidence_level.starts_with("0.8")) return 4;
+	if (confidence_level.starts_with("0.90")) return 5;
+	if (confidence_level.starts_with("0.95")) return 6;
+	if (confidence_level.starts_with("0.98")) return 7;
+	if (confidence_level.starts_with("0.99")) return 8;
+	if (confidence_level.starts_with("0.998")) return 9;
+	if (confidence_level.starts_with("0.999")) return 10;
+	printf("Confidence level not recognized\n");
+	exit(1);
+}
+
+std::tuple<float, float> t_dist(size_t n, float mean, float stddev, float confidence_level = 0.95) {
+	unsigned degrees_of_freedom = (n - 1);
+	if (degrees_of_freedom > 29) {
+		printf("Error: Number of samples exceed optimal number for t-distribution");
+		exit(1);
+	}
+	std::string string = std::to_string(confidence_level);
+	std::string conf_as_str = string + ((string.size() >= 5) ? "" : std::string(5 - string.size(), '0'));
+	float critical_value = t_table[degrees_of_freedom][getTailProbabilityIndex(conf_as_str)];
+	float margin_of_error = stddev / (float)(sqrt(n));
+	float low = mean - (critical_value * margin_of_error);
+	float high = mean + (critical_value * margin_of_error);
+	return std::tuple{ low, high };
+}
+//.......................................................................................................................................................
